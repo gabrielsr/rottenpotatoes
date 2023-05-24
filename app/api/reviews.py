0f93149a68bffa9de75082016bab8fce77e02069
Gabriel Rodrigues <gabrielsr@gmail.com>
2023-05-24 19:09:56 -0300

    Fix api documentation

--- app/api/reviews.py
+++ app/api/reviews.py
@@ -5,18 +5,18 @@ from flask_restx import Api
 blueprint = Blueprint('api', __name__)
 api = Api(blueprint, doc='/doc/')
 
-from ..models import Movie, Review
+from ..models import Movie
 
-model = api.model('Review', {
+review_api_model = api.model('MovieReview', {
     'potatoes': fields.Integer(required=True, description='The number of potatoes'),
-    'moviegoer_name': fields.String(required=True, description='The name of the moviegoer')
+    'moviegoer_name': fields.String(required=True, description='The name of the reviewer')
 })
 
 @api.route('/movies/<int:movie_id>/reviews')
 @api.doc(params={'movie_id': 'ID of a movie'})
 class Reviews(Resource):
     
-    @api.marshal_with(model, envelope='resource')
+    @api.marshal_with(review_api_model, as_list=True)
     def get(self, movie_id):
         return Movie.query.get_or_404(movie_id).reviews
 
